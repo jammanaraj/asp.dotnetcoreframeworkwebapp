b5d7260e1f64cfb9519f8340ee1db1b1fcd07698
vm <53164841+jammanaraj@users.noreply.github.com>
2023-04-29 03:48:16 +0000

    addg

---
+++ dotnetwebapp.WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace dotnetwebapp.WEB.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
